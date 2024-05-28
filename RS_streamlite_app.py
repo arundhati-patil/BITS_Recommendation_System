b4e178ebcbf2d792e90c68206a772188d35665a9
arundhati-patil <144917698+arundhati-patil@users.noreply.github.com>
2024-05-27 21:42:30 -0700

    Update RS_streamlite_app.py

--- RS_streamlite_app.py
+++ RS_streamlite_app.py
@@ -36,7 +36,7 @@ def recommend(movie):
     return recommended_movie_names,recommended_movie_posters
 
 
-st.header(' VANI HANAMSHETTY Movie Recommender System')
+st.header(' Vani Hanamshetty's Movie Recommender System')
 movies = pickle.load(open('movie_list_1.pkl','rb'))
 similarity = pickle.load(open('similarity.pkl','rb'))
 
